6640ab433a60032794694a012c036a8f533a56c2
kweonsikyung <ekrk6632@naver.com>
2024-05-06 04:06:02 +0900

    fix: modify to receive weight prop in ProgressBar

--- src/app/_components/atoms/ProgressBar/ProgressBar.tsx
+++ src/app/_components/atoms/ProgressBar/ProgressBar.tsx
@@ -5,6 +5,8 @@ import { WEIGHT } from '@/app/utils';
 export interface ProgressBarProps {
   /** active될 현재 단계*/
   curr: number;
+  /** 총 단계 수*/
+  weight: number;
   /** 컴포넌트로 생성할 요소의 클래스명 */
   className?: string;
 }
@@ -12,8 +14,8 @@ export interface ProgressBarProps {
 /**
  * WEIGHT(상수)에서 현재 단계 갯수만큼 active, 나머지를 inactive로 표현하는 프로그레스 컴포넌트
  */
-const ProgressBar = ({ curr, className }: ProgressBarProps) => {
-  const arr = Array.from({ length: WEIGHT }, (_, index) =>
+const ProgressBar = ({ curr, className, weight }: ProgressBarProps) => {
+  const arr = Array.from({ length: weight }, (_, index) =>
     Number(index) < Number(curr) ? 1 : 0,
   );
 
