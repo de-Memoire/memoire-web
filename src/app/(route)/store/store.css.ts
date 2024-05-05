66e28a5a2df11bcd24bbe69e4e6b8fc3ec8095e5
kweonsikyung <ekrk6632@naver.com>
2024-05-06 03:54:45 +0900

    feat: `/store` layout & add: `getTemporary()`

---
+++ src/app/(route)/store/store.css.ts
@@ -0,0 +1,16 @@
+import { TEXT_STYLES, COLORS } from '@/app/_constant';
+import { style, styleVariants } from '@vanilla-extract/css';
+
+export const title = style({
+  ...TEXT_STYLES.brand2R.PC,
+  whiteSpace: 'pre-line',
+});
+
+export const desc = style({
+  ...TEXT_STYLES.cap1L.PC,
+  color: COLORS.grayscale.gray7,
+  width: '100%',
+  textAlign: 'right',
+  padding: '10px',
+  cursor: 'pointer',
+});
