45210b78ab7db66fb86bd026e11fcce9129c69ba
kweonsikyung <ekrk6632@naver.com>
2024-05-06 04:03:57 +0900

    fix: Modify related issues in feedbackTag componen

--- src/app/_components/atoms/FeedbackTag/FeedbackTag.tsx
+++ src/app/_components/atoms/FeedbackTag/FeedbackTag.tsx
@@ -9,23 +9,27 @@ export interface FeedbackTagProps {
   /** 피드백 요소 클릭 콜백 함수 */
   onSelect?: (selectedTag: string) => void;
   /** 이미 선택된 피드백 요소 판단 flag */
-  isSelected? : boolean;
+  isSelected?: boolean;
   /** 컴포넌트로 생성할 요소의 클래스명 */
   className?: string;
-  
 }
 
 /**
  * 텍스트 지정 가능한 피드백 태그 요소 컴포넌트
  */
-const FeedbackTag = ({ text, className, isSelected=false, onSelect }: FeedbackTagProps) => {
+const FeedbackTag = ({
+  text,
+  className,
+  isSelected = false,
+  onSelect,
+}: FeedbackTagProps) => {
   const [selected, setSelected] = useState<boolean>(isSelected);
 
- const handleClick = () => {
+  const handleClick = () => {
     if (onSelect) {
       setSelected(!selected);
-      onSelect(text); 
-    } 
+      onSelect(text);
+    }
   };
   return (
     <div
--- src/app/_components/molecules/FeedbackTag/FeedbackTagList/FeedbackTagList.tsx
+++ src/app/_components/molecules/FeedbackTag/FeedbackTagList/FeedbackTagList.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import * as styles from './FeedbackTagList.css';
 import { FeedbackTag } from '../../../atoms';
-
+import { FeedbackTagProps } from '@/app/_data/storydummy';
 export interface FeedbackTagListProps {
   /** 피드백 태그 데이터 */
-  feedbackTagList: string[];
+  feedbackTagList: FeedbackTagProps[];
   /** 피드백 요소 클릭 콜백 함수 */
-  onSelect: (selectedTag: string) => void;
+  onSelect: (selectedTag: FeedbackTagProps) => void;
   /** 컴포넌트로 생성할 요소의 클래스명 */
   className?: string;
 }
@@ -22,7 +22,11 @@ export const FeedbackTagList = ({
   return (
     <div className={`${styles.wrap} ${className}`}>
       {feedbackTagList.map((el) => (
-        <FeedbackTag key={el} text={el} onSelect={() => onSelect(el)} />
+        <FeedbackTag
+          key={el.id}
+          text={el.value}
+          onSelect={() => onSelect(el)}
+        />
       ))}
     </div>
   );
